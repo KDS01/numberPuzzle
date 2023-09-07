b68694e29022c6e0b7a60278e85c65cd2d1bd60e
KDS01 <rlagnek12@naver.com>
2023-09-08 00:21:43 +0900

    end

--- TodoList/src/Todolist/Main.java
+++ TodoList/src/Todolist/Main.java
@@ -3,6 +3,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Scanner;
 import java.util.HashSet;
@@ -15,6 +16,7 @@ import java.util.TreeMap;
 public class Main {
 	public static void main(String[] args) {
 		TreeMap<Integer,String> treemap = new TreeMap<>(Collections.reverseOrder());
+		HashMap<String,String> treemap2 = new HashMap<>();
 		TodoList a = new TodoList();
 		HashSet<String> set = new HashSet<>();
 		TreeSet<Integer> intset = new TreeSet<>();
@@ -29,13 +31,16 @@ public class Main {
 		Scanner scr= new Scanner(System.in);
 		System.out.println("내가 해야 할 일을 입력하자." + "그만두려면 끝을 입력하시오");
 		input = scr.nextLine();
-		if(input.equals("끝")) 
-		{
-			break;
+		if(input.equals("끝")) {
+		while(true) {
+			String Name;
+			System.out.println("해야 할 일의 정보를 보고 싶다면 이름을 입력하시오.");
+			Name = scr.nextLine();
+			System.out.println(treemap2.get(Name));
+			}
 		}
 		System.out.println("그 일의 우선 순위를 입력하자");
 		priority=scr.nextInt();
-		
 		System.out.println("그 일에 대한 설명은?");
 		Scanner ex= new Scanner(System.in);
 		Explanation=ex.nextLine();
@@ -44,11 +49,20 @@ public class Main {
 		inlist.setExplanation(Explanation);
 		inlist.setPriority(priority);
 		treemap.put(priority, inlist.getName());
+		treemap2.put(input,Explanation);
 		System.out.println(treemap);
+		
+		
+			
+			
+			
 		}
-	}
+		}
+		
+		
 	
-}
+	}
+
 		
 		
 		
